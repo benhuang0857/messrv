10727fdba2570a8a1c305ccf2b7d63296c6849ff
BenHuang <benhuang0857@gmail.com>
2022-10-27 22:33:10 +0800

    Stable version-20221027

--- config/admin.php
+++ config/admin.php
@@ -11,7 +11,7 @@ return [
     | login page.
     |
     */
-    'name' => 'Laravel-admin',
+    'name' => 'MES',
 
     /*
     |--------------------------------------------------------------------------
@@ -22,7 +22,7 @@ return [
     | `img` tag, eg '<img src="http://logo-url" alt="Admin logo">'.
     |
     */
-    'logo' => '<b>Laravel</b> admin',
+    'logo' => '<b>MES</b> system',
 
     /*
     |--------------------------------------------------------------------------

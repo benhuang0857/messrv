d3382cc525d575a4a60b840e5b18f9ec83609fca
benhuang0857 <benhuang0857@gmail.com>
2022-11-27 03:09:37 +0800

    20221127

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
                 <div class="panel-heading">任務</div>
                 <div class="panel-heading">
                     {{-- <a href="?show=all"><span class="badge badge-primary" style="padding:10px">所有</span></a> --}}
-                    <a href="?show=approve"><span class="badge badge-primary" style="padding:10px">等待加工({{count($Data['Batches'])}})</span></a>
+                    <a href="?show=approve"><span class="badge badge-primary" style="padding:10px">等待加工({{$Data['JobNotStartCount']}})</span></a>
                     <a href="?show=process"><span class="badge badge-primary" style="padding:10px">進行中({{$Data['JobProcessCount']}})</span></a>
                     <a href="?show=complete"><span class="badge badge-primary" style="padding:10px">完成({{$Data['JobCmpleteCount']}})</span></a>
                     <a href="?show=starthold"><span class="badge badge-primary" style="padding:10px">暫停({{$Data['JobHoldCount']}})</span></a>

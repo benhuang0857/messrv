988bb7d5becaed1ec5ecbbc5312ee0d3605eabcc
benhuang0857 <benhuang0857@gmail.com>
2022-11-27 03:46:59 +0800

    20221127

--- app/Admin/Controllers/RunsController.php
+++ app/Admin/Controllers/RunsController.php
@@ -149,7 +149,7 @@ class RunsController extends AdminController
                 });
             });
 
-            $("input[name=quantity]").change(function(){
+            $("#quantity").change(function(){
                 var productId = $("select[name=product_id]").val();
 
                 $.ajax({

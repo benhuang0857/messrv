fa16f942ca2b53227f8dcec620ab5ab7d699eea2
BenHuang <benhuang0857@gmail.com>
2023-01-02 03:35:48 +0800

    scanner 20230102

--- resources/views/webcamp.blade.php
+++ resources/views/webcamp.blade.php
@@ -1,42 +1,39 @@
-<!DOCTYPE html>
 <html>
 <head>
-    <title>Instascan</title>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/instascan/1.0.0/index.js" integrity="sha512-QblNATV/gin5FC8tqTM2gfCMBei2qCzTte4O6CxGp8KQ5BgC5vNNGv99uTBvzmq+AFFYFoUNhowGOOJNTIBy6A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <!-- 載入instascan.min.js -->
-</head>
-
+    <title>Html-Qrcode Demo</title>
 <body>
-    <video id="preview"></video>
-    <!-- 詢問是否允許開啟相機後，會顯示在這個元素裡 -->
-    <!-- ---------- -->
-    <!-- 以下程式面 -->
-    <script type="text/javascript">
-    let scanner = new Instascan.Scanner({
-        video: document.getElementById('preview')
-    });
-    // 開啟一個新的掃描
-    // 宣告變數scanner，在html<video>標籤id為preview的地方開啟相機預覽。
-    // Notice:這邊注意一定要用<video>的標籤才能使用，詳情請看他的github API的部分解釋。
-
-    scanner.addListener('scan', function(content) {
-        console.log(content);
-    });
-    //開始偵聽掃描事件，若有偵聽到印出內容。
-
-    Instascan.Camera.getCameras().then(function(cameras) {
-    //取得設備的相機數目
-        if (cameras.length > 0) {
-          ///若設備相機數目大於0 則先開啟第0個相機(程式的世界是從第零個開始的)
-            scanner.start(cameras[0]);
-        } else {
-          //若設備沒有相機數量則顯示"No cameras found";
-          //這裡自行判斷要寫什麼
-            console.error('No cameras found.');
-        }
-    }).catch(function(e) {
-        console.error(e);
-    });
-    </script>
+    <div id="qr-reader" style="width:500px"></div>
+    <div id="qr-reader-results"></div>
 </body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/html5-qrcode/2.3.4/html5-qrcode.min.js" integrity="sha512-k/KAe4Yff9EUdYI5/IAHlwUswqeipP+Cp5qnrsUjTPCgl51La2/JhyyjNciztD7mWNKLSXci48m7cctATKfLlQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script>
+    function docReady(fn) {
+        // see if DOM is already available
+        if (document.readyState === "complete"
+            || document.readyState === "interactive") {
+            // call on next available tick
+            setTimeout(fn, 1);
+        } else {
+            document.addEventListener("DOMContentLoaded", fn);
+        }
+    }
+
+    docReady(function () {
+        var resultContainer = document.getElementById('qr-reader-results');
+        var lastResult, countResults = 0;
+        function onScanSuccess(decodedText, decodedResult) {
+            if (decodedText !== lastResult) {
+                ++countResults;
+                lastResult = decodedText;
+                // Handle on success condition with the decoded message.
+                console.log(`Scan result ${decodedText}`, decodedResult);
+            }
+        }
+
+        var html5QrcodeScanner = new Html5QrcodeScanner(
+            "qr-reader", { fps: 10, qrbox: 250 });
+        html5QrcodeScanner.render(onScanSuccess);
+    });
+</script>
+</head>
 </html>
